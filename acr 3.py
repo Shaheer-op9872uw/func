c73c0500a6d3ded9dea190d1dfb448042cb89bd5
Shaheer-op9872uw <shaheerminhas123@gmail.com>
2024-09-07 20:39:40 +0500

    function

---
+++ acr 3.py
@@ -0,0 +1,6 @@
+def add (P,Q):
+    return P + Q
+def subtract(P, Q):
+    return P - Q
+def multiply(P, Q):
+    return 
---
+++ acti 1.py
@@ -0,0 +1,4 @@
+def well_wishes():
+    print ("how are you")
+    print ("hi")
+well_wishes()
